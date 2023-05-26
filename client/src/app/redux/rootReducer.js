070471224abeec3d43a667e4850bf54e53dbab06
Swapnil Sahoo <workforswapnil06@gmail.com>
2023-05-26 20:10:22 +0530

    Added Counter to Test Redux

--- client/src/app/redux/rootReducer.js
+++ client/src/app/redux/rootReducer.js
@@ -1,16 +1,18 @@
 "use client";
 import { combineReducers } from "@reduxjs/toolkit";
 
-import authReducer from "../features/auth/authSlice";
-import deckReducer from "../features/deck/deckSlice";
-import flashcardReducer from "../features/flashcard/flashcardSlice";
-import errorReducer from "../features/errors/errorSlice";
+import authReducer from "./features/auth/authSlice";
+import deckReducer from "./features/deck/deckSlice";
+import flashcardReducer from "./features/flashcard/flashcardSlice";
+import errorReducer from "./features/errors/errorSlice";
+import counterReducer from "./features/counter/counterSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   deck: deckReducer,
   flashcard: flashcardReducer,
   errors: errorReducer,
+  counter: counterReducer,
 });
 
 export default rootReducer;
