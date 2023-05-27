44d4afaa91498a4a9c63c8aafa73bc94023e3c8c
Pratyush Jena <pratyushjena8@gmail.com>
2023-05-28 00:34:53 +0530

    Navbar

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "../components/Navbar";
 
 import { Providers } from "./redux/provider";
 
@@ -14,7 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["corporate"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#661AE6",
+          "secondary": "#D926AA",
+          "accent": "#1FB2A5",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
   },
 };
