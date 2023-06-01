8dbb8e2bb857632f207a486ecb97112b9e1cba4e
Swapnil Sahoo <workforswapnil06@gmail.com>
2023-06-01 15:56:58 +0530

    Login Page Updated

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Login from "@/components/Login.jsx";
-
+import Image from "next/image";
+import loginImg from "../../public/Images/login.svg";
 const login = () => {
+  const divHeight = {
+    height: `calc(100vh - 64px)`,
+  };
   return (
-    <div className="m-40">
-      <Login />
+    <div class="flex h-screen" style={divHeight}>
+      <div class="hidden md:flex w-1/2 items-center justify-center">
+        <Image src={loginImg} alt="" width={500} height={500} />
+      </div>
+      <div class="w-full md:w-1/2 flex items-center justify-center">
+        <Login />
+      </div>
     </div>
   );
 };
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,8 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "../app/redux/features/auth/authSlice";
-
+import Image from "next/image";
+const navbarHeight = 64;
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -49,54 +50,101 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center ">
-      <div className="text-accent text-2xl font-bold mb-4">Login</div>
-      <form className="w-64" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="email" className="text-lg">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="border border-gray-300 rounded px-3 py-2 w-full"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="text-lg">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="border border-gray-300 rounded px-3 py-2 w-full"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Login
-        </button>
-      </form>
-      {/* If login fails, display an error message */}
-
-      {message && <p className="text-red-500 mt-4">{message}</p>}
-
-      <div className="flex flex-col items-center justify-center">
-        <div className="mt-4 text-xs md:text-base w-full text-center">
-          <div className="flex flex-col items-center sm:flex-row sm:justify-center">
-            <span className="whitespace-nowrap">Don't have an account ?
-            </span>
-            <a href="/register" className="text-blue-500 hover:text-blue-700">
-              Register
-            </a>
+    <div class="w-3/4 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Create and account
+        </h1>
+        <form class="space-y-4 md:space-y-6" action="#">
+          <div>
+            <label
+              for="email"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@company.com"
+              required=""
+            />
           </div>
-        </div>
+          <div>
+            <label
+              for="password"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            />
+          </div>
+          <div>
+            <label
+              for="confirm-password"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Confirm password
+            </label>
+            <input
+              type="confirm-password"
+              name="confirm-password"
+              id="confirm-password"
+              placeholder="••••••••"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            />
+          </div>
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
+              <input
+                id="terms"
+                aria-describedby="terms"
+                type="checkbox"
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                required=""
+              />
+            </div>
+            <div class="ml-3 text-sm">
+              <label
+                for="terms"
+                class="font-light text-gray-500 dark:text-gray-300"
+              >
+                I accept the{" "}
+                <a
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  href="#"
+                >
+                  Terms and Conditions
+                </a>
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Create an account
+          </button>
+          <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+            Already have an account?{" "}
+            <a
+              href="#"
+              class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+            >
+              Login here
+            </a>
+          </p>
+        </form>
       </div>
     </div>
   );
