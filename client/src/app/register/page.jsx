aba9ed0ffac9745aaab17e623f85f9b646cdaae3
Swapnil Sahoo <workforswapnil06@gmail.com>
2023-06-01 16:24:29 +0530

    Modified Register and Login Page

--- client/src/app/register/page.jsx
+++ client/src/app/register/page.jsx
@@ -2,11 +2,20 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Register from "../../components/Register";
-
+import Image from "next/image";
+import RegisterImg from "../../public/Images/register.svg";
 const page = () => {
+  const divHeight = {
+    height: `calc(100vh - 64px)`,
+  };
   return (
-    <div className="m-20">
-      <Register />
+    <div class="flex h-screen" style={divHeight}>
+      <div class="w-full md:w-1/2 flex items-center justify-center">
+        <Register />
+      </div>
+      <div class="hidden md:flex w-1/2 items-center justify-center">
+        <Image src={RegisterImg} alt="" width={500} height={500} />
+      </div>
     </div>
   );
 };
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -53,9 +53,9 @@ const LoginPage = () => {
     <div class="w-3/4 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Create and account
+          Sign in to your account
         </h1>
-        <form class="space-y-4 md:space-y-6" action="#">
+        <form class="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
           <div>
             <label
               for="email"
@@ -70,6 +70,7 @@ const LoginPage = () => {
               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="name@company.com"
               required=""
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div>
@@ -86,62 +87,22 @@ const LoginPage = () => {
               placeholder="••••••••"
               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required=""
+              onChange={(e) => setPassword(e.target.value)}
             />
-          </div>
-          <div>
-            <label
-              for="confirm-password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Confirm password
-            </label>
-            <input
-              type="confirm-password"
-              name="confirm-password"
-              id="confirm-password"
-              placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required=""
-            />
-          </div>
-          <div class="flex items-start">
-            <div class="flex items-center h-5">
-              <input
-                id="terms"
-                aria-describedby="terms"
-                type="checkbox"
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                required=""
-              />
-            </div>
-            <div class="ml-3 text-sm">
-              <label
-                for="terms"
-                class="font-light text-gray-500 dark:text-gray-300"
-              >
-                I accept the{" "}
-                <a
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  href="#"
-                >
-                  Terms and Conditions
-                </a>
-              </label>
-            </div>
           </div>
           <button
             type="submit"
-            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-accent hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
           >
-            Create an account
+            Sign In
           </button>
           <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-            Already have an account?{" "}
+            Don’t have an account yet?{" "}
             <a
-              href="#"
+              href="/register"
               class="font-medium text-primary-600 hover:underline dark:text-primary-500"
             >
-              Login here
+              Sign Up
             </a>
           </p>
         </form>
